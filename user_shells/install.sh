18975d57b47d7361b27e6aebabe7fadf616d11a2
Ryota Funaki <ryota.funaki@ngv.jp>
2024-07-12 07:12:33 +0900

    Improve build

--- user_shells/install.sh
+++ user_shells/install.sh
@@ -5,19 +5,5 @@
 # Execute all scripts in the shells.d directory
 TARGET_FILES=$(find shells.d -type f -name "*.sh" | sort)
 for file in $TARGET_FILES; do
-    echo "Executing $file"
     bash "$file"
 done
-
-# Clean up
-sudo apt clean
-sudo rm -rf /var/lib/apt/lists/*
-
-# Add path to the .bashrc file
-cat << EOT >> ~/.bashrc
-
-# Add .NET tools to the PATH
-if [ -d "$HOME/.dotnet/tools" ]; then
-    export PATH="$PATH:$HOME/.dotnet/tools"
-fi
-EOT
